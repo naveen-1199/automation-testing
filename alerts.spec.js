c4adca8ab8db3f2c6245de7996905777bb32ba7d
naveen-1199 <naveensuju@gmail.com>
2026-09-19 11:42:48 +0530

    modified for QA2

--- alerts.spec.js
+++ alerts.spec.js
@@ -17,18 +17,12 @@ await page.goto("https://demoqa.com/alerts");
 //  } )
 //  await page.locator('#timerAlertButton').click();
 
-//  page.on('dialog', async dialog =>{
-//     expect(dialog.type()).toContain("confirm");
-//      expect(dialog.message()).toContain("Do you confirm action?");
-//      await dialog.dismiss();
-//  })
-//  await page.locator('#confirmButton').click();
-
 page.on('dialog', async dialog =>{
     expect(dialog.type()).toContain("prompt");
     expect(dialog.message()).toContain("Please enter your name");
     await dialog.accept("NAVEEN");
 })
  await page.locator('#promtButton').click();
+ await page.screenshot({path:'screenshot/'+ Date.now()+'Normalss.png'});
  await page.waitForTimeout(5000);
 })
